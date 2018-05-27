0bda291ae9b30671425df85889ab14c8ef744554
joelakwam <jakwam@gmail.com>
2018-05-27 21:32:02 +0000

    Add routes to .vtl files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,3 +3,17 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+public class App{
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+
+        get("/", (request, respond) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+    }
+}
